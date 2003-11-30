87649b743052b224a9356a56a260a7a8ff0c6621
d <>
2003-11-30 06:21:46 +0000

    more autoconf work

--- include/see/type.h
+++ include/see/type.h
@@ -21,7 +21,9 @@ typedef unsigned int	  SEE_uint16_t;
 #endif
 
 /* 32-bit signed integer */
-#if SIZEOF_SIGNED_INT == 4
+#if SIZEOF_SIGNED_SHORT == 4
+typedef signed short	  SEE_int32_t;
+#elif SIZEOF_SIGNED_INT == 4
 typedef signed int	  SEE_int32_t;
 #elif SIZEOF_SIGNED_LONG == 4
 typedef signed long	  SEE_int32_t;
@@ -30,12 +32,36 @@ typedef signed long	  SEE_int32_t;
 #endif
 
 /* 32-bit unsigned integer */
-#if SIZEOF_UNSIGNED_INT == 4
+#if SIZEOF_UNSIGNED_SHORT == 4
+typedef unsigned short	  SEE_uint32_t;
+#elif SIZEOF_UNSIGNED_INT == 4
 typedef unsigned int	  SEE_uint32_t;
 #elif SIZEOF_UNSIGNED_LONG == 4
 typedef unsigned long	  SEE_uint32_t;
 #else
 # error "cannot provide type for SEE_uint32_t"
+#endif
+
+/* 64-bit signed integer */
+#if SIZEOF_SIGNED_INT == 8
+typedef signed int	  SEE_int64_t;
+#elif SIZEOF_SIGNED_LONG == 8
+typedef signed long	  SEE_int64_t;
+#elif SIZEOF_SIGNED_LONG_LONG == 8
+typedef signed long long  SEE_int64_t;
+#else
+# error "cannot provide type for SEE_int64_t"
+#endif
+
+/* 64-bit unsigned integer */
+#if SIZEOF_UNSIGNED_INT == 8
+typedef unsigned int	  SEE_uint64_t;
+#elif SIZEOF_UNSIGNED_LONG == 8
+typedef unsigned long	  SEE_uint64_t;
+#elif SIZEOF_UNSIGNED_LONG_LONG == 8
+typedef unsigned long long SEE_uint64_t;
+#else
+# error "cannot provide type for SEE_uint64_t"
 #endif
 
 /* 64-bit floating point */
