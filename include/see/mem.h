623cf9528fb74e78df9c2253fccecdbcb3cdeb62
d <>
2004-03-29 20:19:50 +0000

    style

--- include/see/mem.h
+++ include/see/mem.h
@@ -15,7 +15,7 @@ void  	SEE_free(struct SEE_interpreter *i, void *ptr);
 #define SEE_NEW(i, t)		(t *)SEE_malloc(i, sizeof (t))
 #define SEE_NEW_ARRAY(i, t, n)	(t *)SEE_malloc(i, (n) * sizeof (t))
 
-void* (*SEE_mem_malloc_hook)(struct SEE_interpreter *i, unsigned int sz);
+void *(*SEE_mem_malloc_hook)(struct SEE_interpreter *i, unsigned int sz);
 void  (*SEE_mem_free_hook)(struct SEE_interpreter *i, void *ptr);
 void  (*SEE_mem_exhausted_hook)(struct SEE_interpreter *i) SEE_dead;
 
