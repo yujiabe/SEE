56a813a4355e3ba7b0906e0a0af80344083c3f54
d <>
2004-03-29 20:05:36 +0000

    introduce a third level to the intern cache; for global constant strings

--- include/see/intern.h
+++ include/see/intern.h
@@ -7,8 +7,9 @@
 struct SEE_interpreter;
 struct SEE_string;
 
-struct SEE_string *SEE_intern(struct SEE_interpreter *i, struct SEE_string *s);
-
 void SEE_intern_init(struct SEE_interpreter *i);
+
+struct SEE_string *SEE_intern(struct SEE_interpreter *i, struct SEE_string *s);
+void SEE_intern_global(struct SEE_string *s);
 
 #endif /* _h_intern_ */
