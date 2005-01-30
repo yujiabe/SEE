18ae8b5c15c75842eaf04217f4f9e1b7e791d5e2
d <>
2005-01-30 09:11:38 +0000

    fix ID tag

--- shell/getopt.c
+++ shell/getopt.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* $Id */
+/* $Id$ */
 
 /*
  * A cheap implementation of getopt(), based on API described in
