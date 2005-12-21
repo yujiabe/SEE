f66769eb0b428b51aeed1ec5844f6df231394eb7
d <>
2005-12-21 13:38:17 +0000

    add regression test for bug 24

--- shell/test/regress.js
+++ shell/test/regress.js
@@ -9,5 +9,6 @@ var lower = "abcdefghij0123xyz\uff5a";
 var upper = "ABCDEFGHIJ0123XYZ\uff3a";
 test(literal(lower)+".toUpperCase()", upper)
 test(literal(upper)+".toLowerCase()", lower)
+test("'foo'.lastIndexOf('o', 0)", -1)
 
 finish()
