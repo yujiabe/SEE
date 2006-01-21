1f8201bf230fe3f4c77bab666fe20ab42ce900aa
d <>
2006-01-21 13:13:42 +0000

    a printf implementation for systems that don't have vsnprintf

---
+++ libsee/printf.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2006, David Leonard. All rights reserved. */
+/* $Id$ */
+
+#ifndef _SEE_h_printf
+#define _SEE_h_printf
+
+#include <stdarg.h>
+struct SEE_string;
+struct SEE_interpreter;
+
+void _SEE_vsprintf(struct SEE_interpreter *interp, struct SEE_string *,
+			  const char *fmt, va_list ap);
+void _SEE_sprintf(struct SEE_interpreter *interp, struct SEE_string *,
+			  const char *fmt, ...);
+
+#endif /* _SEE_h_printf */
