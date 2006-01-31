c451a48381c8edcdb0ffd874d303eda37476bb65
d <>
2006-01-31 21:10:59 +0000

    bug 39: dtoa config support for ia64, and alpha (from Simun Mikecin)

--- libsee/dtoa_config.h
+++ libsee/dtoa_config.h
@@ -12,7 +12,7 @@
 
 #include <see/type.h>
 
-#if defined(__i386__) || defined(__amd64__)
+#if defined(__i386__) || defined(__amd64__) || defined(__ia64__) || defined(__alpha__)
 #   define IEEE_8087
 #endif
 
