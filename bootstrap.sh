cbaa754f9b99c52d745825a4a8ac054c4ca57f04
d <>
2006-02-03 10:26:07 +0000

    remove gm4 define

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ rm -rf autom4te.cache
 #-- select the right autoconf tools 
 AUTOCONF_VERSION=2.59; export AUTOCONF_VERSION
 AUTOMAKE_VERSION=1.9;  export AUTOMAKE_VERSION
-M4=/usr/local/bin/gm4; export M4
+#M4=/usr/local/bin/gm4; export M4
 autoreconf --install --force
 
 (cd unicode && make)
