2901b7483e4f74f78f362aa0a19cab4db4e9a7c7
d <>
2006-02-19 14:13:44 +0000

    improve how bootstrap.sh works

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,22 @@
 # $Id$
 # Bootstrap the auxilliary files that don't need to be kept in CVS
 
-set -ex
 
 #-- delete any previous cache
 rm -rf autom4te.cache
 
 #-- select the right autoconf tools 
-AUTOCONF_VERSION=2.59; export AUTOCONF_VERSION
-AUTOMAKE_VERSION=1.9;  export AUTOMAKE_VERSION
+if test x"$AUTOCONF_VERSION" = x""; then
+	AUTOCONF_VERSION=2.59; export AUTOCONF_VERSION
+	echo "export AUTOCONF_VERSION=$AUTOCONF_VERSION" >&2
+fi
+if test x"$AUTOMAKE_VERSION" = x""; then
+	AUTOMAKE_VERSION=1.9; export AUTOMAKE_VERSION
+	echo "export AUTOMAKE_VERSION=$AUTOMAKE_VERSION" >&2
+fi
+
 #M4=/usr/local/bin/gm4; export M4
+set -ex
 autoreconf --install --force
 
 (cd unicode && make)
