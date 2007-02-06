14d6e41033815fd73a57c6bb953715780de9f69d
d <>
2007-02-06 22:34:08 +0000

    correct call, add licence

--- libsee/dtoa_native.c
+++ libsee/dtoa_native.c
@@ -1,7 +1,41 @@
+/*
+ * Copyright (c) 2007
+ *      David Leonard.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Mr Leonard nor the names of the contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY DAVID LEONARD AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL DAVID LEONARD OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+/* $Id: value.c 1142 2006-08-24 09:17:24Z d $ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include "dtoa.h"
 
 /*
- * Pass dtoa() calls through to the native dtoa() in libc.
+ * Pass dtoa() et al. calls through to the native implementation.
  */
 
 double strtod(const char *s00, char **se)
@@ -17,7 +51,7 @@ SEE_strtod(const char *s00, char **se)
 char *
 SEE_dtoa(double d, int mode, int ndigits, int *decpt, int *sign, char **rve)
 {
-	return SEE_dtoa(d, mode, ndigits, decpt, sign, rve);
+	return dtoa(d, mode, ndigits, decpt, sign, rve);
 }
 
 void
